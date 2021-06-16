2cc921e012d3530e20b8e114577008b738b1b993
yennhi <yennhi97263@gmail.com>
2021-06-16 14:36:55 +0700

    - feat: init tailwindcss

---
+++ components/header/header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <div className='bg-red-800 w-40 h-full'>header</div>
+    )
+}
---
+++ components/main/main.tsx
@@ -0,0 +1,5 @@
+export default function Main() {
+    return (
+        <div className='bg-green-500'>main</div>
+    )
+}
